3ff988be7d0944fde836251bbef0c91e28218ffe
yuwx <yuwx@jiguang.cn>
2019-12-07 20:13:16 +0800

    build: 1. eject 2. 初始化InputNumber

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import Icon from './react-ui/Icon';
+import Input from './react-ui/Input';
+import InputNumber from './react-ui/InputNumber';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class  App extends React.Component{
+	constructor(props) {
+		super(props);
+		this.state ={
+			value: 'a'
+		}
+	}
+	value = '1'
+
+	render() {
+		return (
+			<div>
+				<h3>Input 受控</h3>
+				<Input
+				       value={this.state.value}
+				       onChange={(e) => {
+									this.setState({
+										value: e.target.value
+									})
+				}}
+				/>
+				<h3>Input 不受控</h3>
+				<Input
+					rule={/\d/}
+					message="只允许输入数字"
+					defalutValue={this.value}
+					onChange={(e) => {
+						this.value = e.target.value
+					}}/>
+				<h3>InputNumber 受控</h3>
+				<InputNumber
+					rule={/\d/}
+					message="只允许输入数字"
+					defalutValue={this.value}
+					onChange={(e) => {
+						this.value = e.target.value
+					}}/>
+			</div>
+		);
+	}
 }
+
+
+
+
 
 export default App;
